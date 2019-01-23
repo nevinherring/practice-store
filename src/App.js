9d78a16916fde666a3c3ebce626dd072b50d4052
Nevin Herring <nevin.herring@gmail.com>
2019-01-22 22:47:46 -0600

    finished components for now

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import {Switch, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ProductList from './components/ProductList';
+import Navbar from './components/Navbar';
+import Cart from './components/Cart';
+import Details from './components/Details';
+import Default from './components/Default';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <h3>Hello from App</h3>
+        <Navbar />
+        <Switch>
+          <Route path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+        </Switch>
+        <Cart />
+        <Default />
+
       </React.Fragment>
     );
   }
