94f46cf4218f889b36b296640ee29d0d7ca676f6
Nevin Herring <nevin.herring@gmail.com>
2019-01-23 22:23:02 -0600

    finished initial routes and navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Switch, Route} from 'react-router-dom';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ProductList from './components/ProductList';
 import Navbar from './components/Navbar';
@@ -12,14 +12,13 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <h3>Hello from App</h3>
         <Navbar />
         <Switch>
-          <Route path="/" component={ProductList} />
           <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route path="/" component={ProductList} />
+          <Route component={Default} />
         </Switch>
-        <Cart />
-        <Default />
 
       </React.Fragment>
     );
